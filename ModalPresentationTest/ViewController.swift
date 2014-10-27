9185f5ab79ad0d9b14954aae774dc247c5445627
Kolja Kube <code@koljaku.be>
2014-10-28 00:25:02 +0100

    Initial Commit

---
+++ ModalPresentationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModalPresentationTest
+//
+//  Created by Kolja Kube on 28/10/14.
+//  Copyright (c) 2014 Kolja Kube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
